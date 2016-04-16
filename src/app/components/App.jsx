d9dfd4e554593f234eac72dd6d83c2d7bd713fab
Edison Chee <edison_chee@ida.gov.sg>
2016-04-17 02:31:35 +0800

    Added profile photo to quote rendered; need img url in quote.photo

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -8,7 +8,7 @@ export default class App extends React.Component {
 
     var timeNow = Math.floor(Date.now() / 1000);
 
-    this.state = { quote: { created_at: timeNow, user: {name: 'account'}, text: 'Waiting for quote...' } };
+    this.state = { quote: { created_at: timeNow, user: {name: 'account'}, text: 'Waiting for quote...', photo: '' } };
   }
 
   componentDidMount() {
--- src/app/components/Quote.jsx
+++ src/app/components/Quote.jsx
@@ -5,6 +5,7 @@ export default class Quote extends React.Component {
   render() {
     return (
       <div>
+        <div><img className="profilePhoto" src={this.props.quote.photo} /></div>
         <h1>@{this.props.quote.user.name} <Time quote={this.props.quote} /></h1>
         <p>{this.props.quote.text}</p>
       </div>
