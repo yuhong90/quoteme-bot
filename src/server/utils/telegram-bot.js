30b9b6ee275260450efb58dd8ae9fcbb1a5b79f0
Shek Yu Hong <shek_yu_hong@ida.gov.sg>
2016-04-24 12:08:42 +0800

    Prioritised new quotes to always display first; new quotes will now be queued instead of displayed;

--- src/server/utils/telegram-bot.js
+++ src/server/utils/telegram-bot.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = process.env['TELEGRAM_TOKEN_2'];
+//const token = process.env['TELEGRAM_TOKEN_2'];
+const token = require('./settings').speakerbotToken;
 const telegram_filepath = 'https://api.telegram.org/file/bot';
 const bot = new TelegramBot(token, {polling: true});
 const Promise = require('bluebird');
@@ -11,6 +12,7 @@ bot.setup = function(_io){
 
 var timeout, timeInterval;
 var quotesArr = [];
+var newQuotesArr = [];
 const maxQuoteCount = 30;
 
 bot.getMe().then(function (me) {
@@ -21,11 +23,12 @@ bot.onText(/\/start/, function (msg) {
   var fromName = msg.from.first_name + ' ' + msg.from.last_name;
   var chatId = msg.chat.id;
   var opts = {reply_to_message_id: msg.message_id};
-  bot.sendMessage(chatId, 'Welcome ' + fromName + '. Get started with /quote.', opts);
+  bot.sendMessage(chatId, 'Welcome ' + fromName + '. Get started with /quote <your quotes here>.', opts);
 });
 
 bot.onText(/\/help/, function (msg) {
   var chatId = msg.chat.id;
+  var fromName = getUsername(msg);
   var opts = {
     reply_to_message_id: msg.message_id,
     reply_markup: JSON.stringify({
@@ -34,11 +37,11 @@ bot.onText(/\/help/, function (msg) {
         ['/echo - echo your msg to yourself']]
       })
     };
-    bot.sendMessage(chatId, 'Help me help you.', opts);
+    bot.sendMessage(chatId, 'Help me help you, @' + fromName, opts);
 });
 
-bot.onText(/\/quote (.+)/, function (msg, match) {
-  var fromName = msg.from.last_name;
+bot.onText(/\/quote (.+)/, function (msg, match) {  
+  var fromName = getUsername(msg); 
   var user = msg.from.id;
   var time = msg.date;
   var text = match[1];
@@ -50,24 +53,25 @@ bot.onText(/\/quote (.+)/, function (msg, match) {
 
     bot.getFile(fileid).then(function(file){
       var photoURL = telegram_filepath + bot.token +'/'+ file.file_path;
-      sendQuote(fromName, text, time, photoURL, '');
+      sendQuoteIfBoardEmpty(user, fromName, text, time, photoURL, '');
+      sendMsgToUser(user, getQuoteResponse());
+
       storeQuote(user, fromName, text, time, photoURL, '');
-      sendMsgToUser(user, 'Done! Your quote will be shown on the board!');
     });
   });
 });
 
 bot.onText(/\/echo (.+)/, function (msg, match) {
-    console.log('/echo : ' + msg.from.id + match[1]);
+    console.log('/echo : ' + msg.from.username + match[1]);
   var fromId = msg.from.id;
-  var fromName = msg.from.last_name;
+  var fromName = getUsername(msg);
   var resp = match[1];
-  bot.sendMessage(fromId, fromName + ': ' + resp);
+  bot.sendMessage(fromId, fromName + ': mic test. ' + resp);
 });
 
 bot.on('photo', function (msg) {
   var user = msg.from.id;
-  var fromName = msg.from.last_name;
+  var fromName = getUsername(msg);
   var photo = msg.photo[1].file_id;
   var caption = msg.caption;
   var time = msg.date;
@@ -76,28 +80,30 @@ bot.on('photo', function (msg) {
     var fileid = profilePhoto.photos[0][1].file_id;
 
     Promise.join(
-    bot.getFileLink(photo).then(function(fileURI){
-      console.log('promised' + fileURI);
-      return fileURI;
-    }),
+      bot.getFileLink(photo).then(function(fileURI){
+        console.log('promised' + fileURI);
+        return fileURI;
+      }),
 
-    bot.getFile(fileid).then(function(file){
-      var photoURL = telegram_filepath + bot.token +'/'+ file.file_path;
-      return photoURL;
-    }),
+      bot.getFile(fileid).then(function(file){
+        var photoURL = telegram_filepath + bot.token +'/'+ file.file_path;
+        return photoURL;
+      }),
     function(photoURI, profilePic) {
-      storeQuote(user, fromName, caption, time, profilePic, photoURI);
-      sendQuote(fromName, caption, time, profilePic, photoURI);
       console.log('photoURI: ' + photoURI + '\n' + 'profilePic: ' + profilePic);
+      sendQuoteIfBoardEmpty(user, fromName, caption, time, profilePic, photoURI)
+      sendMsgToUser(user, getQuoteResponse());
+      storeQuote(user, fromName, caption, time, profilePic, photoURI); 
     })
   });
+
 });
 
 bot.start = function startRandomQuotes(){
   var timeNow = Math.floor(Date.now() / 1000);
   //start cycling thru quotes after 5 secs
   timeout = setTimeout(function(){
-    sendQuote('speakerbot', 'board ready', timeNow, '', '');
+    sendQuote('speakerbot', 'board ready - Send your quotes with /quotes <your quotes>!', timeNow, '', '');
     timeInterval = setInterval(RandomQuote, 10000);
   }, 5000);
 }
@@ -107,13 +113,41 @@ bot.stop = function stopRandomQuotes(){
   clearInterval(timeInterval);
 }
 
-function sendQuote(user, content, time, profilePic, photo){
-  var quote =  {  user: {name: user, profilePic: profilePic}, text: content, created_at: time, photo: photo};
-  io.emit('quote sent', quote);
+function getUsername(msg){
+  var username = msg.from.username;
+  var firstlastname = msg.from.first_name + ' ' + msg.from.last_name;
+
+  if (username !== ''){
+    return username;
+  }
+  return firstlastname;
 }
 
-function sendMsgToUser(fromId, resp){
-  bot.sendMessage(fromId, resp);
+function getQuoteResponse(){
+  if (newQuotesArr.length === 0){
+    return 'Done! Your quote will be shown on the board now!';
+  }else{
+    return 'Done! Your quote will be queued behind ' + newQuotesArr.length + ' quote(s)!';
+  }
+}
+
+function checkQuoteArrEmpty(){
+  return (quotesArr.length === 0);
+}
+
+function sendQuoteIfBoardEmpty(id, user, content, time, profilePic, photo){ 
+  if (checkQuoteArrEmpty()){
+    console.log('new quote sent, board was empty');
+    sendQuote(user, content, time, profilePic, photo);  
+  }else{
+    console.log('new quote queued');
+    queueNewQuote(id, user, content, time, profilePic, photo);
+  }
+}
+
+function sendQuote(user, content, time, profilePic, photo){
+  var quote =  { user: {name: user, profilePic: profilePic}, text: content, created_at: time, photo: photo};
+  io.emit('quote sent', quote);
 }
 
 function storeQuote(user, name, content, time, profilePic, photo){
@@ -121,16 +155,36 @@ function storeQuote(user, name, content, time, profilePic, photo){
      quotesArr.shift();
   }
   var len = quotesArr.push({id:user, name:name, profilePic: profilePic, content:content, created_at: time, photo: photo});
-  console.log('quote count: ' + len);
+  console.log('stored quote count: ' + len);
 }
 
-function RandomQuote(){
-  var len = quotesArr.length;
-  if (len > 1){
-    var rand = Math.floor((Math.random() * len));
-    // sendMsgToUser(quotesArr[rand].user, 'your quote is shown now!');
-    sendQuote(quotesArr[rand].name, quotesArr[rand].content, quotesArr[rand].created_at, quotesArr[rand].profilePic, quotesArr[rand].photo);
+function queueNewQuote(user, name, content, time, profilePic, photo){ 
+  var len = newQuotesArr.push({id:user, name:name, profilePic: profilePic, content:content, created_at: time, photo: photo});
+  console.log('newQuotes in queue: ' + len);
+}
+
+function clearNewQuoteQueue(){
+  if (newQuotesArr.length > 0){
+      var quote = newQuotesArr.shift();
+      console.log('newQuotes left: ' + newQuotesArr.length);
+      sendQuote(quote.name, quote.content, quote.created_at, quote.profilePic, quote.photo);
+      // sendMsgToUser(quotesArr[rand].user, 'your quote is shown now!');
+      return true;
   }
+  return false;
+}
+
+function RandomQuote(){ 
+  var len = quotesArr.length;
+    if (!clearNewQuoteQueue() && len > 1){
+      var rand = Math.floor((Math.random() * len));
+      console.log('randQuote: ' + rand);
+      sendQuote(quotesArr[rand].name, quotesArr[rand].content, quotesArr[rand].created_at, quotesArr[rand].profilePic, quotesArr[rand].photo);      
+    }
+}
+
+function sendMsgToUser(fromId, resp){
+  bot.sendMessage(fromId, resp);
 }
 
 module.exports = bot;
